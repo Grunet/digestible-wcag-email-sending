da90b7881c56f595c9b709a57875134a4c546a26
Grunet <sadikshahidain@gmail.com>
2020-07-08 21:26:16 -0500

    Adding in a factory that adapts the SendGrid dependence

---
+++ src/emailClientFactory.js
@@ -0,0 +1,72 @@
+const SendGrid = "SendGrid";
+
+const supportedClients = {
+  [SendGrid]: SendGrid,
+};
+
+function createEmailClient(clientId, defaultSettings) {
+  if (!supportedClients.hasOwnProperty(clientId)) {
+    throw new Error(
+      `Unrecognized client id passed to factory: ${clientId.toString()}`,
+    );
+  }
+
+  switch (clientId) {
+    case SendGrid:
+      if (!defaultSettings.hasOwnProperty("apiKey")) {
+        throw new Error(
+          "Required API key is missing from the passed SendGrid default settings",
+        );
+      }
+
+      return new SendGridAdapter(defaultSettings);
+      break;
+  }
+}
+
+class SendGridAdapter {
+  constructor(defaults) {
+    this.__sgMail = require("@sendgrid/mail");
+    this.__sgMail.setApiKey(defaults["apiKey"]);
+
+    this.__defaultSettings = { ...defaults }; //Shallow copy
+  }
+
+  async send(msgData) {
+    const msgDataWithDefaults = Object.assign(
+      { ...this.__defaultSettings },
+      msgData,
+    ); //Shallow copy
+
+    const { to, from, subject, html } = msgDataWithDefaults;
+
+    const msgDataToSend = {
+      to: to,
+      from: from,
+      subject: subject,
+      html: html,
+    };
+
+    for (const [key, value] of Object.entries(msgDataToSend)) {
+      if (!value) {
+        throw new Error(
+          `"${key}" is missing from the inputs to the SendGrid mail sending api`,
+        );
+      }
+    }
+
+    try {
+      await this.__sgMail.send(msgDataToSend);
+    } catch (error) {
+      console.error(error);
+      console.error(error?.response?.body);
+
+      throw error;
+    }
+  }
+}
+
+module.exports = {
+  Clients: supportedClients,
+  createEmailClient: createEmailClient,
+};
