aa0cbde92e44f11f975ab09d224c365629cfe64e
Grunet <sadikshahidain@gmail.com>
2020-07-12 21:05:34 -0500

    Pulling in the recipients from AWS

---
+++ src/recipientsRetrieval.js
@@ -0,0 +1,20 @@
+const { getSubscribers } = require("digestible-wcag-contact-management");
+
+async function getRecipients() {
+  const subscribers = await getSubscribers();
+
+  const recipients = Array.from(subscribers).map((accountObj) => {
+    const { email: address, ...rest } = accountObj;
+    const adaptedAccountObj = { emailAddress: address, ...rest };
+
+    return adaptedAccountObj;
+  });
+
+  return {
+    recipients: new Set(recipients),
+  };
+}
+
+module.exports = {
+  getRecipients: getRecipients,
+};
