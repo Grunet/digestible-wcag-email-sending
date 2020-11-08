a213574d943ed7e4885f7c7cc8557d364b971f36
Grunet <sadikshahidain@gmail.com>
2020-11-07 18:37:18 -0600

    Implementing the (authorized) query against the new subscribers API

--- src/recipientsRetrieval.js
+++ src/recipientsRetrieval.js
@@ -1,3 +1,6 @@
+require("node-fetch"); //amazon-cognito-identity-js uses the fetch API and needs a polyfill to be in place
+const AmazonCognitoIdentity = require("amazon-cognito-identity-js");
+
 async function getRecipients(inputs) {
   const getSubscribers =
     inputs?.dependencies?.getSubscribers ??
@@ -15,6 +18,103 @@ async function getRecipients(inputs) {
   return {
     recipients: new Set(recipients),
   };
+}
+
+async function __getSubscribers(inputs) {
+  const { auth: authInfo, path } = inputs;
+
+  const { cognitoUser, authenticationDetails } = __hydrateCognitoObjects(
+    authInfo
+  );
+  const jwtAccessToken = await __tryAuthenticatingAgainstCognito(
+    cognitoUser,
+    authenticationDetails
+  );
+
+  const resObj = await __queryContactsApiForSubscribers(path, jwtAccessToken);
+
+  const {
+    data: { subscribers },
+  } = resObj;
+
+  return {
+    subscribers: new Set(subscribers),
+  };
+}
+
+function __hydrateCognitoObjects(authInfo) {
+  const {
+    credentials: { username, password },
+    identifiers: { appClientId, userPoolId },
+  } = authInfo;
+
+  const cognitoUser = new AmazonCognitoIdentity.CognitoUser({
+    Username: username,
+    Pool: new AmazonCognitoIdentity.CognitoUserPool({
+      UserPoolId: userPoolId,
+      ClientId: appClientId,
+    }),
+  });
+
+  cognitoUser.setAuthenticationFlowType("USER_PASSWORD_AUTH");
+  const authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(
+    {
+      Username: username,
+      Password: password,
+    }
+  );
+
+  return {
+    cognitoUser: cognitoUser,
+    authenticationDetails: authenticationDetails,
+  };
+}
+
+async function __tryAuthenticatingAgainstCognito(
+  cognitoUser,
+  authenticationDetails
+) {
+  return new Promise((resolve, reject) => {
+    cognitoUser.authenticateUser(authenticationDetails, {
+      onSuccess: function (result) {
+        const accessToken = result.getAccessToken().getJwtToken();
+
+        resolve(accessToken);
+      },
+      onFailure: function (err) {
+        console.error(err);
+
+        reject(
+          "Failed to authenticate against the API's AWS Cognito User Pool"
+        );
+      },
+    });
+  });
+}
+
+async function __queryContactsApiForSubscribers(path, jwtAccessToken) {
+  const res = await fetch(path, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${jwtAccessToken}`,
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      //Apollo-Server specifics
+      "Accept-Encoding": "gzip, deflate, br",
+      Connection: "keep-alive",
+    },
+    body: JSON.stringify({
+      query: `{ 
+        subscribers { 
+          email
+        } 
+      }
+    `,
+    }),
+  });
+  const resObj = await res.json();
+
+  return resObj;
 }
 
 module.exports = {
