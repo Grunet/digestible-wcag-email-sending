b31931f4fc23fa05399266cfbc05e51bb63bfb8e
Grunet <sadikshahidain@gmail.com>
2020-12-01 19:57:54 -0600

    Rate limiting the email sending (since SES comes with one and will be switched over to shortly)

--- src/retrieveThenSend.js
+++ src/retrieveThenSend.js
@@ -1,3 +1,7 @@
+const Bottleneck = require("bottleneck");
+
+const SENDING_RATE = 10; //per second. Slightly less than my current AWS SES limit. Generalize as needed.
+
 async function retrieveThenSend(dependencies) {
   const { getRecipients, getTemplate, sendEmail } = dependencies;
 
@@ -17,9 +21,15 @@ async function retrieveThenSend(dependencies) {
       };
     });
 
+    const limiter = new Bottleneck({
+      minTime: 1000 / SENDING_RATE,
+    });
+
     const emailSendingOutcomes = await Promise.allSettled(
       msgDataList.map((msgData) => {
-        return sendEmail(msgData);
+        return limiter.schedule(async () => {
+          return await sendEmail(msgData);
+        });
       })
     );
 
--- test/unit/automated/retrieveThenSend_test.js
+++ test/unit/automated/retrieveThenSend_test.js
@@ -110,3 +110,40 @@ test("Sends emails to the rest of the recipients even if sending fails for some"
   expect(spies.console.error).toHaveBeenCalledTimes(1);
   expect(sendEmailSuccessfully).toHaveBeenCalledTimes(2 - 1);
 });
+
+test("Sends at most 14 emails per second to avoid exceeding AWS SES rate limits", async () => {
+  //ARRANGE
+  const NUM_RECIPIENTS = 15;
+
+  const getRecipients = jest.fn().mockReturnValue({
+    recipients: new Set(
+      Array.from({ length: NUM_RECIPIENTS }, (v, i) => i).map((i) => ({
+        emailAddress: `${i}@example.org`,
+      }))
+    ),
+  });
+  const getTemplate = jest.fn().mockReturnValue({
+    html: "<div>Email content</div>",
+    subject: "Email subject",
+  });
+
+  //This assumes that if this mock is called repeatedly in a loop, it will execute very quickly (<<1 sec) and break the rate limit
+  const sendInstants = [];
+  const sendEmail = jest.fn().mockImplementation(async () => {
+    sendInstants.push(Date.now());
+  });
+
+  //ACT
+  await retrieveThenSend({
+    getRecipients: getRecipients,
+    getTemplate: getTemplate,
+    sendEmail: sendEmail,
+  });
+
+  //ASSERT
+  const averageSendingRate =
+    sendInstants.length /
+    ((Math.max(...sendInstants) - Math.min(...sendInstants)) / 1000);
+
+  expect(averageSendingRate).toBeLessThan(14); //per second
+});
