b23706724fe7c3703a7554f90d4926568b48b603
Grunet <sadikshahidain@gmail.com>
2020-12-04 00:23:11 -0600

    Factoring out the shared setup parts of the integration test file

--- test/integration/automated/index_test.js
+++ test/integration/automated/index_test.js
@@ -2,55 +2,7 @@ const { sendEmailsToRecipients } = require("../../../dist/index.js");
 
 test("Calls the primary sending provider's API to send an email", async () => {
   //ARRANGE
-  const inputs = {
-    dependencies: {
-      tryGetAuthToken: jest.fn(),
-      getSubscribers: jest.fn().mockReturnValue({
-        subscribers: [
-          {
-            email: "recipientAddress@example.com",
-          },
-        ],
-      }),
-      getTemplateData: jest.fn().mockReturnValue({
-        html: "<div>Email Content</div>",
-        plainText: "Email Content",
-        subject: "Email Subject",
-      }),
-      ses: {
-        sendEmail: jest.fn(),
-      },
-      sendGrid: {
-        send: jest.fn(),
-      },
-    },
-    apiKeys: {
-      sendGrid: { sendOnly: "SG.unused" },
-    },
-    credentials: {
-      cognito: {
-        contactsApiAuth: {
-          username: "unused",
-          password: "unused",
-        },
-      },
-    },
-    emails: {
-      sender: "senderAddress@example.com",
-    },
-    ids: {
-      cognito: {
-        contactsApiAuth: {
-          appClientId: "unused",
-          userPoolId: "unused",
-        },
-      },
-    },
-    urls: {
-      currentSelectionServer: "unused",
-      contactsApi: "unused",
-    },
-  };
+  const inputs = __setupDefaultInputs();
 
   //ACT
   await sendEmailsToRecipients(inputs);
@@ -61,59 +13,12 @@ test("Calls the primary sending provider's API to send an email", async () => {
 
 test("Calls the fallback sending provider's API to send an email after the primary's API failed to", async () => {
   //ARRANGE
-  const inputs = {
-    dependencies: {
-      tryGetAuthToken: jest.fn(),
-      getSubscribers: jest.fn().mockReturnValue({
-        subscribers: [
-          {
-            email: "recipientAddress@example.com",
-          },
-        ],
-      }),
-      getTemplateData: jest.fn().mockReturnValue({
-        html: "<div>Email Content</div>",
-        plainText: "Email Content",
-        subject: "Email Subject",
-      }),
-      ses: {
-        sendEmail: jest
-          .fn()
-          .mockRejectedValue(
-            Error("Simulated SES sending error - i.e. this was hit on purpose")
-          ),
-      },
-      sendGrid: {
-        send: jest.fn(),
-      },
-    },
-    apiKeys: {
-      sendGrid: { sendOnly: "SG.unused" },
-    },
-    credentials: {
-      cognito: {
-        contactsApiAuth: {
-          username: "unused",
-          password: "unused",
-        },
-      },
-    },
-    emails: {
-      sender: "senderAddress@example.com",
-    },
-    ids: {
-      cognito: {
-        contactsApiAuth: {
-          appClientId: "unused",
-          userPoolId: "unused",
-        },
-      },
-    },
-    urls: {
-      currentSelectionServer: "unused",
-      contactsApi: "unused",
-    },
-  };
+  const inputs = __setupDefaultInputs();
+  inputs.dependencies.ses.sendEmail = jest
+    .fn()
+    .mockRejectedValue(
+      Error("Simulated SES sending error - i.e. this was hit on purpose")
+    );
 
   //ACT
   await sendEmailsToRecipients(inputs);
@@ -125,7 +30,19 @@ test("Calls the fallback sending provider's API to send an email after the prima
 
 test("Doesn't send any emails if the sender's email isn't provided", async () => {
   //ARRANGE
-  const inputs = {
+  const inputs = __setupDefaultInputs();
+  inputs.emails.sender = undefined;
+
+  //ACT
+  await sendEmailsToRecipients(inputs);
+
+  //ASSERT
+  expect(inputs.dependencies.ses.sendEmail).toHaveBeenCalledTimes(0);
+  expect(inputs.dependencies.sendGrid.send).toHaveBeenCalledTimes(0);
+});
+
+function __setupDefaultInputs() {
+  return {
     dependencies: {
       tryGetAuthToken: jest.fn(),
       getSubscribers: jest.fn().mockReturnValue({
@@ -159,7 +76,7 @@ test("Doesn't send any emails if the sender's email isn't provided", async () =>
       },
     },
     emails: {
-      sender: undefined,
+      sender: "senderAddress@example.com",
     },
     ids: {
       cognito: {
@@ -174,11 +91,4 @@ test("Doesn't send any emails if the sender's email isn't provided", async () =>
       contactsApi: "unused",
     },
   };
-
-  //ACT
-  await sendEmailsToRecipients(inputs);
-
-  //ASSERT
-  expect(inputs.dependencies.ses.sendEmail).toHaveBeenCalledTimes(0);
-  expect(inputs.dependencies.sendGrid.send).toHaveBeenCalledTimes(0);
-});
+}
